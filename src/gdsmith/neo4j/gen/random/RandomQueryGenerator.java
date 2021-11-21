58d65ad326ecb4890cdbecbf76861462a55893d5
abc123000111 <lin_444@live.com>
2021-11-21 13:26:40 +0800

    使用递归随机生成sketch

--- src/gdsmith/neo4j/gen/random/RandomQueryGenerator.java
+++ src/gdsmith/neo4j/gen/random/RandomQueryGenerator.java
@@ -1,5 +1,6 @@
 package gdsmith.neo4j.gen.random;
 
+import gdsmith.Randomly;
 import gdsmith.cypher.CypherQueryAdapter;
 import gdsmith.neo4j.Neo4jGlobalState;
 import gdsmith.neo4j.ast.ClauseSequence;
@@ -11,18 +12,33 @@ import gdsmith.neo4j.schema.Neo4jSchema;
 
 public class RandomQueryGenerator {
 
+    private ClauseSequenceBuilder generateClauses(ClauseSequenceBuilder seq, int len) {
+        if (len == 0) {
+            return seq;
+        }
+        boolean generateWith = Randomly.getBooleanWithRatherLowProbability();
+        //boolean generateWith = Randomly.getBoolean();
+        if (generateWith) {
+            return generateClauses(seq.WithClause(), len - 1);
+        } else {
+            return generateClauses(seq.MatchClause(), len - 1);
+        }
+    }
+
     public ClauseSequence generateQuery(Neo4jGlobalState globalState){
         ClauseSequenceBuilder builder = new ClauseSequenceBuilder();
-
         Neo4jSchema schema = globalState.getSchema();
+        Randomly r = new Randomly();
 
-        //示例：使用ClauseSequenceBuilder流式生成query，在build时需要传入条件，别名，模式的生成策略，以及全局符号表
-        ClauseSequence sequence = builder.MatchClause().MatchClause().
-                ReturnClause().build(new NaiveConditionGenerator(schema),
-                new NaiveAliasGenerator(schema, builder.getIdentifierBuilder()),
-                new NaivePatternGenerator(schema, builder.getIdentifierBuilder()), schema);
-
-
+        int numOfClauses = r.getInteger(1, 10);
+        /* int numOfClauses = Randomly.smallNumber();
+        if (numOfClauses <= 0) {
+            numOfClauses = 1;
+        } */
+        ClauseSequence sequence = generateClauses(builder, numOfClauses).ReturnClause().build(new RandomConditionGenerator(schema),
+                new RandomAliasGenerator(schema, builder.getIdentifierBuilder()),
+                new RandomPatternGenerator(schema, builder.getIdentifierBuilder()), schema);
+        
         return sequence;
     }
 }
