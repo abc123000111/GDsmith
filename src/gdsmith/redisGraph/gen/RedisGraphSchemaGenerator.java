6c738f6989c266036d1a6ba249613a496d65dc24
abc123000111 <lin_444@live.com>
2021-12-16 10:19:14 +0800

    新增redisgraph的索引生成和图生成

--- src/gdsmith/redisGraph/gen/RedisGraphSchemaGenerator.java
+++ src/gdsmith/redisGraph/gen/RedisGraphSchemaGenerator.java
@@ -23,50 +23,29 @@ public class RedisGraphSchemaGenerator extends CypherSchemaGenerator<RedisGraphS
 
     @Override
     public RedisGraphSchema generateSchemaObject(RedisGraphGlobalState globalState, List<CypherSchema.CypherLabelInfo> labels, List<CypherSchema.CypherRelationTypeInfo> relationTypes, List<CypherSchema.CypherPatternInfo> patternInfos) {
-        /*for (CypherSchema.CypherLabelInfo label: labels) {
-            String createVertex = "CREATE VLABEL ";
-            createVertex += label.getName();
-            //System.out.println(createVertex);
-            try {
-                globalState.executeStatement(new CypherQueryAdapter(createVertex));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        for (CypherSchema.CypherRelationTypeInfo type: relationTypes) {
-            String createEdge = "CREATE ELABEL ";
-            createEdge += type.getName();
-            try {
-                globalState.executeStatement(new CypherQueryAdapter(createEdge));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
         Randomly r = new Randomly();
         int numOfIndexes = r.getInteger(5, 8);
 
         for (int i = 0; i < numOfIndexes; i++) {
-            String createIndex = "CREATE PROPERTY INDEX ON ";
+            String createIndex = "CREATE INDEX ON :";
             if (Randomly.getBoolean()) {
                 CypherSchema.CypherLabelInfo n = labels.get(r.getInteger(0, labels.size() - 1));
-                createIndex = createIndex + n.getName() + " (";
+                createIndex = createIndex + n.getName() + "(";
                 IPropertyInfo p = n.getProperties().get(r.getInteger(0, n.getProperties().size() - 1));
                 createIndex = createIndex + p.getKey() + ")";
             } else {
                 CypherSchema.CypherRelationTypeInfo re = relationTypes.get(r.getInteger(0, relationTypes.size() - 1));
-                createIndex = createIndex + re.getName() + " (";
+                createIndex = createIndex + re.getName() + "(";
                 IPropertyInfo p = re.getProperties().get(r.getInteger(0, re.getProperties().size() - 1));
                 createIndex = createIndex + p.getKey() + ")";
             }
-            //System.out.println(createIndex);
+            System.out.println(createIndex);
             try {
                 globalState.executeStatement(new CypherQueryAdapter(createIndex));
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }*/
+        }
 
         return new RedisGraphSchema(new ArrayList<>(), labels, relationTypes, patternInfos);
     }
